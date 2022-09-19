f4c71e376a29dffafd5627f0a16e620d1bf21a8d
Brayan Esneider Hernandez Martinez <brayanhernandezmartinez@gmail.com>
2022-09-19 15:31:32 -0500

    First commit, basic api

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import *
+import json, time, requests, random, os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home_page():
+    adult_query = str(request.args.get('adult'))
+    year = str(request.args.get('year'))
+    page = random.randint(1, 500)
+    api_url = f'https://api.themoviedb.org/3/discover/movie?api_key={get_Env()}&include_adult={adult_query}&page={page}&year={year}'
+    response = requests.get(api_url)
+    data = json.dumps(response.json())
+    y = json.loads(data)
+    z = y["results"]
+    random_number = random.randint(1,len(z))-1
+    return response.json()
+
+@app.route('/user/', methods=['GET'])
+def request_page():
+    user_query = str(request.args.get('user'))
+
+    data_set = {'Page': 'Request', 'Message': f'Successfully got the request for {user_query}', 'Timestamp': time.time()}
+    json_dump = json.dumps(data_set)
+
+    return json_dump
+
+def get_Env():
+    return os.getenv('PYTHON_API_MOVIE')
+
+if __name__ == '__main__':
+    app.run(port=7777)
